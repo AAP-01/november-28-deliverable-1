86e549bd36f223a2c9d3fb436b0a58e36f47bbf8
DanKo1003 <koivunendan10@gmail.com>
2025-12-02 13:36:09 -0500

    Recipe Base

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] arg) {
-        System.out.println("World Hello!");
+        Recipe start = new Recipe(1, "Cake");
+        start.addIngredient(new Ingredient("Sugar", QuantityTypes.tableSpoon, 3));
+        start.addIngredient(new Ingredient("Flour", QuantityTypes.cup, 2));
+        start.printIngredients();
     }
 }
--- Recipe.java
+++ Recipe.java
@@ -6,13 +6,28 @@ public class Recipe
     String recipeName;
     int id;
 
-    public Recipe()
+    public Recipe(int id, String recipeName)
     {
-
+        this.id = id;
+        this.recipeName = recipeName;
+        ingredientList = new ArrayList<>();
+        System.out.println("Created: " + recipeName + ", ID: " + id);
     }
     
     public void printIngredients()
     {
+        if (ingredientList != null)
+        {
+            for (Ingredient ingredient: ingredientList)
+            {
+                System.out.println(ingredient.getIngredientText());
+            }
+        }
+        
+    }
 
+    public void addIngredient(Ingredient ingredient) 
+    {
+        ingredientList.add(ingredient);
     }
 }
