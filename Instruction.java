379d25a1157425d7a2f68d80bcce85caa75b7a0c
AAP-01 <jbthememer43@gmail.com>
2025-12-11 17:46:18 -0500

    JAVADOC for intruction

--- Instruction.java
+++ Instruction.java
@@ -11,11 +11,17 @@ public class Instruction
     String instruction;
     ArrayList<String> stepsList;
 
+    /**
+     * This constructor creates a new Arraylist for the steps.
+     */
     public Instruction()
     {
         stepsList = new ArrayList<>();
     }
     
+    /**
+     * printSteps() prints all the steps in the ArrayList in the terminal.
+     */
     public void printSteps()//shows the added steps in terminal
     {
         int index = 1;
@@ -26,6 +32,9 @@ public class Instruction
         }
     }
     
+    /**
+     * getSteps() gets all the steps in the info variable.
+     */
     public String getSteps()
     {
         int index = 1;
@@ -38,16 +47,32 @@ public class Instruction
         return info;
     }
 
+    /**
+     * addStep(String step, int index) manually adds a step in the list in 
+     * a given position.
+     * @param step The step.
+     * @param index The position of the step in the list.
+     */
     public void addStep(String step, int index)//add a step in a particular position (ie manually)
     {
         stepsList.add(index, step);
     }
 
+    /**
+     * addStep(String step) adds a step in the list in the most recent 
+     * position.
+     * @param step The step.
+     */
     public void addStep(String step)
     {
         stepsList.add(step);
     }
 
+    /**
+     * removeStep(int index) removes the step linked to the inputted index 
+     * (the step's position in the list).
+     * @param index The position of the step in the list.
+     */
     public void removeStep(int index)
     {
         stepsList.remove(index);        
